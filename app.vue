7169524b00f05bb50036a62130789a44a2fad517
yussan <main.yussan@gmail.com>
2023-08-24 13:48:05 +0700

    style(init quasar): quasar ready

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div><NuxtPage /></div>
 </template>
---
+++ components/commons/navigations/global-header/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <q-toolbar class="bg-white text-gray">
+      <!-- <q-btn flat round dense icon="menu" class="q-mr-sm" /> -->
+      <q-img src="/assets/images/mcdg-logo-large.svg" width="40px" />
+
+      <q-toolbar-title><strong>MauCoding</strong></q-toolbar-title>
+      <q-tabs class="mcdg-tab" v-model="tab" shrink>
+        <q-tab class="q-pa-md" name="tab1" label="HOME" />
+        <q-tab class="q-pa-md" name="tab2" label="POSTS" />
+        <q-tab class="q-pa-md" name="tab3" label="VIDEOS" />
+      </q-tabs>
+      <q-btn flat round dense icon="search" />
+    </q-toolbar>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tab: "tab1",
+    };
+  },
+};
+</script>
