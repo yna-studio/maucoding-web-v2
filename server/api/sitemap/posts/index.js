4a2e33e0cb163da5d3d6db176d91a3c9b07a1a00
yussan <main.yussan@gmail.com>
2023-09-02 17:04:36 +0700

    feat(sitemap): test sitemap/posts

--- server/api/sitemap/posts/index.js
+++ server/api/sitemap/posts/index.js
@@ -1,6 +1,6 @@
 import { sitemapGenerator } from "../../../helpers/XMLGenerator";
 
 export default defineEventHandler(async (event) => {
-  const params = [{ link: "/232" }];
+  const params = [{ link: "/232" }, { link: "/232" }];
   return sitemapGenerator(params);
 });
