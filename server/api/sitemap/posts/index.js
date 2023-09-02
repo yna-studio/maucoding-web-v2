0e8cf9e14e737c750966cfbadc6f02bf7c22c3a1
yussan <main.yussan@gmail.com>
2023-09-02 17:18:49 +0700

    force update

--- server/api/sitemap/posts/index.js
+++ server/api/sitemap/posts/index.js
@@ -1,4 +1,4 @@
-import { sitemapGenerator } from "../../../helpers/XMLGenerator";
+// import { sitemapGenerator } from "../../../helpers/XMLGenerator";
 
 export default defineEventHandler(async (event) => {
   const params = [{ link: "/232" }, { link: "/232" }, { link: "/122" }];
